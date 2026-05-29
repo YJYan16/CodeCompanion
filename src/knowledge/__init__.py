f4158a7e0515d6abddc15f6bb28431b133b2a606
YJYan16 <1438114498@qq.com>
2026-05-29 12:54:35 +0800

    调整

--- src/knowledge/__init__.py
+++
@@ -1,4 +0,0 @@
-from .graph_builder import KnowledgeGraph
-from .graph_data import KnowledgeNodes, ErrorNodes
-
-__all__ = ['KnowledgeGraph', 'KnowledgeNodes', 'ErrorNodes']
--- src/languages/__init__.py
+++
@@ -1,8 +0,0 @@
-# src/languages/__init__.py
-from .python_parser import PythonParser
-from .java_parser import JavaParser
-
-LANGUAGE_PARSERS = {
-    "python": PythonParser(),
-    "java": JavaParser()
-}
