db0d63a7679d27679669f0b84e2924dec137066c
YJYan16 <83354165+YJYan16@users.noreply.github.com>
2026-05-30 19:23:29 +0800

    Delete 作业 directory

--- 作业/孙七.py
+++
@@ -1,9 +0,0 @@
-def find_max(numbers)    # 这里缺少冒号！
-    max_num = 0
-    for num in numbers:   # 这一行也缺少冒号
-        if num > max_num  # 也缺少冒号
-            max_num = num
-    return max_num
-
-# 测试
-print(find_max([1, 5, 3]))
